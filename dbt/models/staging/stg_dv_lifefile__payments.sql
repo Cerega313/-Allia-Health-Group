cf485c291e3d04ac9cad768269761acbfced6d94
Sergei Grigorev <85104260+Cerega313@users.noreply.github.com>
2025-11-14 19:26:27 +0000

    Update stg_dv_lifefile__payments.sql

--- dbt/models/staging/stg_dv_lifefile__payments.sql
+++ dbt/models/staging/stg_dv_lifefile__payments.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
+{{ config(materialized = 'view') }}
 
 {%- set yaml_metadata -%}
 source_model:
@@ -7,16 +7,42 @@ source_model:
 ldts: updated_at
 rsrc: "!lifefile/payments"
 
+business_keys:
+  - payment_id
+  - order_id
+  - payer_id
+
 hashed_columns:
   hk_payment_h:
     - payment_id
 
+  hk_order_h:
+    - order_id
+
+  hk_payer_h:
+    - payer_id
+
+  # Satellites
   hd_payment_financials_s:
     - copay_amount
     - insurance_amount
     - total_amount
     - cost_amount
     - profit_amount
+
+  hd_payment_context_s:
+    - payment_date
+    - posting_date
+    - status
+    - payment_method
+    - payer_type
+    - is_refund
+    
+  hd_payment_technical_s:
+    - created_at
+    - gcs_uri
+
 {%- endset -%}
 
-{{ datavault4dbt.stage(yaml_metadata=yaml_metadata) }}
+{{ datavault4dbt.stage(yaml_metadata = yaml_metadata) }}
+
