824d73bbb3b64d001c670b759f70107e3ee52370
Sergei Grigorev <85104260+Cerega313@users.noreply.github.com>
2025-11-14 19:46:54 +0000

    Create link_payment_order_l.sql

---
+++ dbt/models/raw_vault/link_payment_order_l.sql
@@ -0,0 +1,12 @@
+{{ config(materialized = 'incremental') }}
+
+{%- set yaml_metadata -%}
+link_hashkey: 'hk_payment_order_l' 
+foreign_hashkeys:
+  - 'hk_payment_h'
+  - 'hk_order_h' 
+source_models:
+  - name: stg_dv_lifefile__payments
+{%- endset -%}
+
+{{ datavault4dbt.link(yaml_metadata = yaml_metadata) }}
